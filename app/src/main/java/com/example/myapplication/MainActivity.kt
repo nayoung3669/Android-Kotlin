394d368de78dddb93d3199d550c4c9f71f071b9e
nayoung3669 <skdud3669@gmail.com>
2024-04-18 15:30:12 +0900

    kotlin button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,64 +9,28 @@ import com.example.myapplication.R.drawable
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    lateinit var text1: TextView
-    lateinit var text2: TextView
-    lateinit var chkAgree: CheckBox
-    lateinit var rGroup1: RadioGroup
-    lateinit var rdoDog: RadioButton
-    lateinit var rdoCat: RadioButton
-    lateinit var rdoHam: RadioButton
-    lateinit var rdoBara: RadioButton
-    lateinit var rdoRabbit: RadioButton
-    lateinit var btnOK: Button
-    lateinit var imgPet: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(drawable.ic_launcher)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title = "애완동물 사진 보기"
+//        setContentView(R.layout.activity_main)
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        val baseLayout = LinearLayout(this)
+        baseLayout.orientation = LinearLayout.VERTICAL
+        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0))
+        setContentView(baseLayout, params)
 
-        text1 = findViewById(R.id.Text1) // 검색시작?
-        chkAgree = findViewById(R.id.ChkAgree)
+        val btn = Button(this)
+        btn.text = "버튼입니다."
+        btn.setBackgroundColor(Color.MAGENTA)
+        baseLayout.addView(btn)
 
-        text2 = findViewById(R.id.Text2)
-        rGroup1 = findViewById(R.id.Rgroup1)
-        rdoDog = findViewById(R.id.RdoDog)
-        rdoCat = findViewById(R.id.RdoCat)
-        rdoHam = findViewById(R.id.RdoHam)
-        rdoBara = findViewById(R.id.RdoBara)
-        rdoRabbit = findViewById(R.id.RdoRabbit)
-
-        btnOK = findViewById(R.id.BtnOK)
-        imgPet = findViewById(R.id.ImgPet)
-
-        chkAgree.setOnCheckedChangeListener { compoundButton, b ->
-            if (chkAgree.isChecked == true) {
-                text2.visibility = android.view.View.VISIBLE
-                rGroup1.visibility = android.view.View.VISIBLE
-                btnOK.visibility = android.view.View.VISIBLE
-                imgPet.visibility = android.view.View.VISIBLE
-            } else {
-                text2.visibility = android.view.View.INVISIBLE
-                rGroup1.visibility = android.view.View.INVISIBLE
-                btnOK.visibility = android.view.View.INVISIBLE
-                imgPet.visibility = android.view.View.INVISIBLE
-
-            }
+        btn.setOnClickListener {
+            Toast.makeText(applicationContext, "코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show()
         }
-
-        btnOK.setOnClickListener {
-            when (rGroup1.checkedRadioButtonId) {
-                R.id.RdoDog -> imgPet.setImageResource(R.drawable.dog)
-                R.id.RdoCat -> imgPet.setImageResource(R.drawable.cat)
-                R.id.RdoHam -> imgPet.setImageResource(R.drawable.ham)
-                R.id.RdoBara -> imgPet.setImageResource(R.drawable.bara)
-                R.id.RdoRabbit -> imgPet.setImageResource(R.drawable.rabbit)
-                else -> Toast.makeText(applicationContext, "동물 먼저 선택하세요.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
     }
 }
