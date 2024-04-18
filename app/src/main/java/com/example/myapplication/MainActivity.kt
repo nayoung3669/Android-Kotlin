eba91463858eb8feb595309f84637392db253b96
nayoung3669 <skdud3669@gmail.com>
2024-04-18 15:36:08 +0900

    relative layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,23 +14,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setIcon(drawable.ic_launcher)
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        val params = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
-        )
-        val baseLayout = LinearLayout(this)
-        baseLayout.orientation = LinearLayout.VERTICAL
-        baseLayout.setBackgroundColor(Color.rgb(0, 255, 0))
-        setContentView(baseLayout, params)
+        setContentView(R.layout.activity_main)
 
-        val btn = Button(this)
-        btn.text = "버튼입니다."
-        btn.setBackgroundColor(Color.MAGENTA)
-        baseLayout.addView(btn)
-
-        btn.setOnClickListener {
-            Toast.makeText(applicationContext, "코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show()
-        }
     }
 }
