24a1870a5a9942f68962c15dac267b540b88372f
nayoung3669 <skdud3669@gmail.com>
2024-04-24 16:16:16 +0900

    calculator

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,29 @@ import com.example.myapplication.R.drawable
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+    lateinit internal var edit1: EditText
+    lateinit internal var edit2: EditText
+    lateinit internal var btnAdd: Button
+    lateinit internal var btnSub: Button
+    lateinit internal var btnMul: Button
+    lateinit internal var btnDiv: Button
+    lateinit internal var textResult: TextView
+    lateinit internal var num1: String
+    lateinit internal var num2: String
+    internal var result: Int? = null
+    internal var numButtons = ArrayList<Button>(10)
+    internal var numBtnIDs = arrayOf(
+        R.id.BtnNum0,
+        R.id.BtnNum1,
+        R.id.BtnNum2,
+        R.id.BtnNum3,
+        R.id.BtnNum4,
+        R.id.BtnNum5,
+        R.id.BtnNum6,
+        R.id.BtnNum7,
+        R.id.BtnNum8,
+        R.id.BtnNum9
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.setDisplayShowHomeEnabled(true)
