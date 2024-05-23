b13e9f1d6a23386361eaa4b93e670a1fe93c6127
nayoung3669 <skdud3669@gmail.com>
2024-05-23 14:35:28 +0900

    file read / write

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,62 +1,40 @@
 package com.example.myapplication
-
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        title = "토스트 연습"
+        var btnRead=findViewById<Button>(R.id.btnRead)
+        var btnWrite=findViewById<Button>(R.id.btnWrite)
 
-        var button1 = findViewById<Button>(R.id.button1)
-
-        //기본 대화상자
-//        button1.setOnClickListener {
-//            var dlg = AlertDialog.Builder(this@MainActivity)
-//            dlg.setTitle("제목입니다.")
-//            dlg.setIcon(R.mipmap.ic_launcher)
-//            dlg.setMessage("이곳이 내용입니다.")
-//
-//            dlg.setPositiveButton("확인"){ dialog, which ->
-//                Toast.makeText(this@MainActivity,
-//                    "확인을 눌렀네요", Toast.LENGTH_SHORT).show()
-//            }
-//            dlg.setPositiveButton("확인", null)
-//            dlg.show()
-//        }
-
-        //목록 대화상자
-//        button1.setOnClickListener {
-//            var versionArray = arrayOf("오레오", "파이", "큐(10)")
-//            var dlg = AlertDialog.Builder(this@MainActivity) //dlg.setTitle("제목입니다.")
-//            dlg.setTitle("좋아하는 버전은?")
-//            dlg.setIcon(R.mipmap.ic_launcher)
-//            dlg.setItems(versionArray) { dialog, which ->
-//                button1.text = versionArray[which]
-//            }
-//            dlg.setPositiveButton("닫기", null)
-//            dlg.show()
-//
-//        }
-
-        //동시선택 대화 샂아
-        button1.setOnClickListener {
-            var versionArray = arrayOf("오레오", "파이", "큐(10)")
-            var dlg = AlertDialog.Builder(this@MainActivity)
-            var checkArray = booleanArrayOf(true, false, false)
-            dlg.setTitle("좋아하는 버전은?")
-            dlg.setMultiChoiceItems(versionArray,
-                checkArray) { dialog, which, isChecked ->
-                button1.text = versionArray[which]
-            }
-            dlg.setPositiveButton("닫기", null)
-            dlg.show()
-
+        btnWrite.setOnClickListener{
+            var outFs : FileOutputStream = openFileOutput("file.txt", Context.MODE_PRIVATE)
+            var str =" 쿡북 안드로이드"
+            outFs.write(str.toByteArray())
+            outFs.close()
+            Toast.makeText(applicationContext, "file.txt가 생성됨", Toast.LENGTH_SHORT).show()
         }
+
+        btnRead.setOnClickListener{
+            try {
+                var inFs = openFileInput("file.txt")
+                var txt = ByteArray(30)
+                inFs.read(txt)
+                var str = txt.toString(Charsets.UTF_8)
+                Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+                inFs.close()
+            } catch (e: IOException) {
+                Toast.makeText(applicationContext, "파일 없음", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
