5d867ac35ca3c8f3f10e103f53ea17e0eeb9373b
nayoung3669 <skdud3669@gmail.com>
2024-05-29 21:26:34 +0900

    edit diary

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,70 @@
 package com.example.myapplication
+
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.EditText
 import android.widget.Toast
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dp: DatePicker
+    lateinit var edtDiary: EditText
+    lateinit var btnWrite: Button
+    lateinit var fileName: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "간단 일기장"
 
-        var btnRead=findViewById<Button>(R.id.btnRead)
-        var btnWrite=findViewById<Button>(R.id.btnWrite)
+        dp = findViewById<DatePicker>(R.id.datePicker1)
+        edtDiary = findViewById(R.id.edtDiary)
+        btnWrite = findViewById(R.id.btnWrite)
 
-        btnWrite.setOnClickListener{
-            var outFs : FileOutputStream = openFileOutput("file.txt", Context.MODE_PRIVATE)
-            var str =" 쿡북 안드로이드"
-            outFs.write(str.toByteArray())
-            outFs.close()
-            Toast.makeText(applicationContext, "file.txt가 생성됨", Toast.LENGTH_SHORT).show()
+        var cal = Calendar.getInstance()
+        var cYear = cal.get(Calendar.YEAR)
+        var cMonth = cal.get(Calendar.MONTH)
+        var cDay = cal.get(Calendar.DAY_OF_MONTH)
+
+        dp.init(cYear, cMonth, cDay) { view, year, monthOfYear, dayOfMonth ->
+            fileName = (Integer.toString(year) + "_"
+                    + Integer.toString(monthOfYear + 1) + "_"
+                    + Integer.toString(dayOfMonth) + ".txt")
+            var str = readDiary(fileName)
+            edtDiary.setText(str)
+            btnWrite.isEnabled = true
         }
 
-        btnRead.setOnClickListener{
-            try {
-                var inFs = openFileInput("file.txt")
-                var txt = ByteArray(30)
-                inFs.read(txt)
-                var str = txt.toString(Charsets.UTF_8)
-                Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
-                inFs.close()
-            } catch (e: IOException) {
-                Toast.makeText(applicationContext, "파일 없음", Toast.LENGTH_SHORT).show()
-            }
+        btnWrite.setOnClickListener{
+            var outFs = openFileOutput(fileName, Context.MODE_PRIVATE)
+            var str = edtDiary.text.toString()
+            outFs.write(str.toByteArray())
+            outFs.close()
+            Toast.makeText(applicationContext, "$fileName 이 저장됨", Toast.LENGTH_SHORT).show()
         }
 
     }
+
+    fun readDiary(fName: String): String? {
+        var diaryStr: String? = null
+        var inFs: FileInputStream
+        try {
+            inFs = openFileInput(fName)
+            var txt = ByteArray(500)
+            inFs.read(txt)
+            inFs.close()
+            diaryStr = txt.toString(Charsets.UTF_8).trim()
+            btnWrite.text = "수정하기"
+        } catch (e: IOException) {
+            edtDiary.hint = "일기 없음"
+            btnWrite.text = " tofh wjwkd"
+        }
+        return null
+    }
+
 }
