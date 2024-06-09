26c29a9e637e1351718c967494e44069db5d0072
nayoung3669 <skdud3669@gmail.com>
2024-06-09 15:08:20 +0900

    week 14 hap

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -22,44 +24,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title = "명화 선호도 투표"
+        title = "메인 액티비티"
 
-        var voteCount = IntArray(9)
-        for (i in 0..8)
-            voteCount[i] = 0
-        var image = arrayOfNulls<ImageView>(9)
-        var imageId = arrayOf(
-            R.id.iv1, R.id.iv2, R.id.iv3,
-            R.id.iv4, R.id.iv5, R.id.iv6,
-            R.id.iv7, R.id.iv8, R.id.iv9
-        )
-        var imgName =
-            arrayOf(
-                "name1", "name2", "name3",
-                "name4", "name5", "name6",
-                "name7", "name8", "name9"
-            )
-
-
-        for (i in imageId.indices) {
-            image[i] = findViewById(imageId[i])
-            image[i]!!.setOnClickListener {
-                voteCount[i]++
-                Toast.makeText(
-                    applicationContext,
-                    imgName[i] + ": 총 " + voteCount[i] + "표",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
+        var btnNewActivity = findViewById<Button>(R.id.btnNewActivity)
+        btnNewActivity.setOnClickListener {
+            var edtNum1 = findViewById<EditText>(R.id.edtNum1)
+            var edtNum2 = findViewById<EditText>(R.id.edtNum1)
+            var intent = Intent(applicationContext, SecondActivity::class.java)
+            intent.putExtra("Num1", Integer.parseInt(edtNum1.text.toString()))
+            intent.putExtra("Num2", Integer.parseInt(edtNum2.text.toString()))
+            startActivityForResult(intent, 0)
         }
-        var btnFinish = findViewById<Button>(R.id.btnResult)
+    }
 
-        btnFinish.setOnClickListener {
-            var intent = Intent(applicationContext, ResultActivity::class.java)
-            intent.putExtra("VoteCount", voteCount)
-            intent.putExtra("ImageName", imgName)
-            startActivity(intent)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            var hap = data!!.getIntExtra("Hap", 0)
+            Toast.makeText(applicationContext, "합계 : $hap", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 
@@ -8,10 +9,18 @@ class SecondActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second)
+        title = "Second 액티비티"
 
+        var inIntent = intent
+        var hapValue = inIntent.getIntExtra("Num1", 0) + inIntent.getIntExtra("Num2", 0)
         var btnReturn = findViewById<Button>(R.id.btnReturn)
         btnReturn.setOnClickListener {
+            var outIntent = Intent(applicationContext, MainActivity::class.java)
+            outIntent.putExtra("Hap", hapValue)
+            setResult(Activity.RESULT_OK, outIntent)
             finish()
         }
+
+
     }
 }
